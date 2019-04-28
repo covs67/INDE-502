aa4a23313135f27a1aab6d839cd1665fef05d74e
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-04-28 12:48:06 +0100

    pages creation

--- BlockEasyAppMain/block-easy-react-app/src/App.js
+++ BlockEasyAppMain/block-easy-react-app/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
-function App() {
+
+class App extends Component {
+render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +26,8 @@ function App() {
     </div>
   );
 }
+}
+
+
 
 export default App;
---
+++ BlockEasyAppMain/block-easy-react-app/src/create.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
+
+class create extends component {
+render() {
+return({ <div>This is the create page</div>
+
+});
+
+}
+}
+
+
+export default create;
