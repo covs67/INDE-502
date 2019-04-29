39e2380cb1b461aac2806f753c894ae5d056dcb0
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-04-29 02:36:27 +0100

    images + create + login page content

--- BlockEasyAppMain/block-easy-react-app/src/create.js
+++ BlockEasyAppMain/block-easy-react-app/src/create.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import {AppBar} from '@material-ui/core/';
+import {AppBar, TextField, Button} from '@material-ui/core/';
 
 class Create extends Component {
 
@@ -8,9 +8,27 @@ return(
   <div>
     <AppBar position="static" color="default">
       <div style={{height:'50%',width:'50%',marginTop:'5px'}}>
-        <h1>Login</h1>
+        <h1>Create</h1>
       </div>
     </AppBar>
+<center>
+  <img src={require('./images/blockeasy.JPG')} style={{marginTop:'2%'}}>
+  </img>
+  <div style={{alignItems:'space-between'}}>
+  <TextField label='Name' style={{marginBottom:'1%'}}/>
+  <br/>
+  <TextField label='Email address' style={{marginBottom:'1%'}}/>
+  <br/>
+  <TextField label='Password' style={{marginBottom:'1%'}}/>
+  <br/>
+  <TextField label='Password' protected style={{marginBottom:'1%'}}/>
+  </div>
+  <br/>
+  <div style={{marginTop:'2%'}}>
+  <Button variant='contained' colour='primary' style={{marginRight:'2%'}}>Create</Button>
+  <Button variant='contained' colour='primary'>Cancel</Button>
+  </div>
+</center>
   </div>);
   }
 }
--- BlockEasyAppMain/block-easy-react-app/src/login.js
+++ BlockEasyAppMain/block-easy-react-app/src/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
-import {AppBar} from '@material-ui/core/';
+import {AppBar, Button, TextField} from '@material-ui/core/';
+var img = require("./images/blockeasy.JPG");
 
 class Login extends Component {
 
@@ -8,13 +9,30 @@ render() {
 
 return(
   <div>
+    
   <AppBar position="static" color="default">
         <div style={{height:'50%',width:'50%',marginTop:'5px'}}>
        <h1>Login</h1>
-       <img></img>
        </div>
   </AppBar>
+  <center>
+  <img src = {img} style={{marginTop:'2%'}}
+       />
+  <br/>
+
+  <TextField label='Username' />
+  <br/>
+  <TextField label='Password' protected />
+  <br/>
+  <div style={{marginTop:'3%'}}>
+  <Button variant='contained' color='primary' style={{marginRight:'2%'}}>Login</Button>
+  <Button variant='contained' color='primary'>Create</Button>
+  </div>
+  
+
+  </center>
    </div>
+  
 
 );
 
