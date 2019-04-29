14bdcb5ba2b270295e7e23e7c92a87537f8d4625
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-04-29 03:57:15 +0100

    login validation2

--- BlockEasyAppMain/block-easy-react-app/src/create.js
+++ BlockEasyAppMain/block-easy-react-app/src/create.js
@@ -3,6 +3,17 @@ import {AppBar, TextField, Button} from '@material-ui/core/';
 
 class Create extends Component {
 
+  constructor (props)
+  {super (props)
+  }
+    state={Name:'', Email:'', Password:'', Password2:'', redirectToHome:false};
+   handleRegister(event){
+  // alert(this.state.Name)
+  // alert(this.state.Email)
+  // alert(this.state.Password)
+  // alert(this.state.Password2)
+  }
+
 render() {
 return(
   <div>
@@ -15,17 +26,29 @@ return(
   <img src={require('./images/blockeasy.JPG')} style={{marginTop:'2%'}}>
   </img>
   <div style={{alignItems:'space-between'}}>
-  <TextField label='Name' style={{marginBottom:'1%'}}/>
+  <TextField label='Name' style={{marginBottom:'1%'}} onChange={(event)=> {
+this.setState({Name:event.target.value
+})
+ }}/>
   <br/>
-  <TextField label='Email address' style={{marginBottom:'1%'}}/>
+  <TextField label='Email address' style={{marginBottom:'1%'}} onChange={(event)=> {
+this.setState({Email:event.target.value
+})
+ }}/>
   <br/>
-  <TextField label='Password' style={{marginBottom:'1%'}}/>
+  <TextField label='Password' style={{marginBottom:'1%'}} onChange={(event)=> {
+this.setState({Password:event.target.value
+})
+ }}/>
   <br/>
-  <TextField label='Password' protected style={{marginBottom:'1%'}}/>
+  <TextField label='Password' type='Password2' type='Password' style={{marginBottom:'1%'}} onChange={(event)=> {
+this.setState({Password2:event.target.value
+})
+ }} />
   </div>
   <br/>
   <div style={{marginTop:'2%'}}>
-  <Button variant='contained' colour='primary' style={{marginRight:'2%'}}>Create</Button>
+  <Button variant='contained' colour='primary' style={{marginRight:'2%'}} onClick={(event) => { this.handleRegister(event) }}>Create</Button>
   <Button variant='contained' colour='primary'>Cancel</Button>
   </div>
 </center>
--- BlockEasyAppMain/block-easy-react-app/src/login.js
+++ BlockEasyAppMain/block-easy-react-app/src/login.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
-import {AppBar, Button, TextField} from '@material-ui/core/';
+import {AppBar, Button} from '@material-ui/core/';
+import TextField from 'material-ui/TextField';
 import { checkPropTypes } from 'prop-types';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 var img = require("./images/blockeasy.JPG");
+
 
 class Login extends Component {
 constructor (props)
 {super (props)
 }
-  state={Username:'', Password:'', redirectToHome:false};
+  state={Username:'', Password:'', redirectToHome:false, error:[], password_error:'', //Store password error message
+  username_error:''//Store username error message
+};
  handleRegister(event){
-
+  event.stopPropagation()
+  //To store the errors
+  const errors={}
+  var re =  new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  var emailVer=re.test(String(this.state.Username).toLowerCase())
+  //alert (emailVer)
+  var strongRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
+  var PassVer=strongRegex.test(String(this.state.Password))
+  //Flag to determine that the contents are valid.
+  var flagInvalid=false
+  if(emailVer === false){
+    errors.username_error="Email address is invalid, Re-enter."
+    flagInvalid=true
+  }
+  //Setting the error message if password is invalid.
+  if(PassVer === false){
+    errors.password_error="Password is invalid, Re-enter."
+    flagInvalid=true
+  }
+  //Set messages if data is incorrect showing user errors
+    if(flagInvalid==true){
+      event.preventDefault();
+      this.setState({
+        ...this.state,
+        ...errors
+      })
+    }
 }
 
 
@@ -18,7 +49,7 @@ render() {
 
 return(
   <div>
-    
+   <MuiThemeProvider>
   <AppBar position="static" color="default">
         <div style={{height:'50%',width:'50%',marginTop:'5px'}}>
        <h1>Login</h1>
@@ -29,9 +60,17 @@ return(
        />
   <br/>
   <div>
-  <TextField label='Username' />
+  <TextField label='Username' errorText={this.state.username_error} onChange={(event)=> {
+this.setState({Username:event.target.value
+
+})
+ }}/>
   <br/>
-  <TextField label='Password' protected />
+  <TextField label='Password' errorText={this.state.password_error} type='Password' onChange={(event)=> {
+this.setState({Password:event.target.value
+
+})
+ }}/>
   <br/>
   </div>
   <div style={{marginTop:'3%'}}>
@@ -41,8 +80,9 @@ return(
   
 
   </center>
+  </MuiThemeProvider>
    </div>
-  
+
 
 );
 
