bd597f9e2bd643b88e683bd61929a49a18e20bd9
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-04-29 02:49:42 +0100

    Login Validation1

--- BlockEasyAppMain/block-easy-react-app/src/login.js
+++ BlockEasyAppMain/block-easy-react-app/src/login.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import {AppBar, Button, TextField} from '@material-ui/core/';
+import { checkPropTypes } from 'prop-types';
 var img = require("./images/blockeasy.JPG");
 
 class Login extends Component {
+constructor (props)
+{super (props)
+}
+  state={Username:'', Password:'', redirectToHome:false};
+ handleRegister(event){
+
+}
+
 
 render() {
 
@@ -19,13 +28,14 @@ return(
   <img src = {img} style={{marginTop:'2%'}}
        />
   <br/>
-
+  <div>
   <TextField label='Username' />
   <br/>
   <TextField label='Password' protected />
   <br/>
+  </div>
   <div style={{marginTop:'3%'}}>
-  <Button variant='contained' color='primary' style={{marginRight:'2%'}}>Login</Button>
+  <Button variant='contained' color='primary' style={{marginRight:'2%'}} onClick={(event) => { this.handleRegister(event) }}>Login</Button>
   <Button variant='contained' color='primary'>Create</Button>
   </div>
   
