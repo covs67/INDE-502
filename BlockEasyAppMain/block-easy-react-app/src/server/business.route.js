99dbd84092bfaeaf32c0296236e42fbeef5f869e
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-05-18 23:56:40 +0100

    connectionjs + businessroutes2

--- BlockEasyAppMain/block-easy-react-app/src/server/business.route.js
+++ BlockEasyAppMain/block-easy-react-app/src/server/business.route.js
@@ -2,15 +2,13 @@ const express = require('express');
 const businessRoutes = express.Router();
 
 // Require Business model in our routes module
-let Business = require('./business.model');
+let Business = 
 
 // Define store route
 businessRoutes.route('/add').post(function (req, res) {
     let business = new Business(req.body);
     business.save()
-      .then(business => {
-        res.status(200).json({'userdata': 'user added successfully'});
-      })
+      
       .catch(err => {
       res.status(400).send("unable to send to database");
       });
@@ -19,10 +17,10 @@ businessRoutes.route('/add').post(function (req, res) {
 // Define edit route
 businessRoutes.route('/get/:name/:password').get(function (req, res) {e
     var name = req.params.name;
-    var password = req.params.password;
+    
  var flag = false;
 
- 
+
 
 
   module.exports = businessRoutes;
