c3c4df02699b1510ad3ddd2f5a3d63916749cd1b
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-05-18 23:53:51 +0100

    connectionjs + business routes

---
+++ BlockEasyAppMain/block-easy-react-app/src/connection.js
@@ -0,0 +1,16 @@
+//Get required files.
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./Server/DB.js');
+const businessRoute = require('./Server/business.route.js');
+
+mongoose.Promise = global.Promise;
+//Create server connection
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+  );
