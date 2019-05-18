3af2a95d31e99625cf486e707a238eb1a0a0cd00
VALENTINE <valentine@VALENTINEs-MacBook-Pro-2.local>
2019-05-19 00:16:12 +0100

    businessroute1

--- BlockEasyAppMain/block-easy-react-app/src/server/business.route.js
+++ BlockEasyAppMain/block-easy-react-app/src/server/business.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const businessRoutes = express.Router();
 
 // Require Business model in our routes module
-let Business = require('./business.model');
+let Business = require('./businesses.model');
 
 // Define store route
 businessRoutes.route('/add').post(function (req, res) {
@@ -11,13 +11,11 @@ businessRoutes.route('/add').post(function (req, res) {
       .then(business => {
         res.status(200).json({'userdata': 'user added successfully'});
       })
-      .catch(err => {
-      res.status(400).send("unable to send to database");
-      });
+      
   });
 
 // Define edit route
-businessRoutes.route('/get/:name/:password').get(function (req, res) {e
+businessRoutes.route('/get/:name/:password').get(function (req, res) {
     var name = req.params.name;
     var password = req.params.password;
  var flag = false;
@@ -30,10 +28,19 @@ businessRoutes.route('/get/:name/:password').get(function (req, res) {e
          //check if user exists
          Businesses.forEach(element =>{
              //check if data matches
-             if(element.email)
-         })
+             
+              }
+         });
+         //Send response accordingly
+      
+        res.json({'found':'true'});
+      }
+      else{
+        res.json({'found':'false'});
+      }
      }
  })
+})
 
 
   module.exports = businessRoutes;
